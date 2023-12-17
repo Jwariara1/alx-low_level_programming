0ea2924fb179982e19c4a2e68b3de963e156ae30
Jwariara1 <jwariara1@gmail.com>
2023-12-17 00:18:34 -0800

    4-print_alphabt.c

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Description: a program that prints the alphabet in lowercase
+ * followed by a new line
+ * Return: 0
+ */
+int main(void)
+{
+char c = 'a';
+while (c <= 'z')
+{
+if (c != 'e' && c != 'q')
+{
+putchar(c);
+}
+c++;
+}
+puchar('\n')
+return (0);
+}
