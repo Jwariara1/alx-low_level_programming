2939c296dc6e314ab30b25cd0178b052005bc186
Jwariara1 <jwariara1@gmail.com>
2023-12-18 12:48:28 -0800

    101-print_comb4.c

--- 0x01-variables_if_else_while/101-print_comb4.c
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -9,16 +9,34 @@
  */
 int main(void)
 {
-int c = 0;
-int sum = 0;
-while (c < 1024)
+int c;
+int d;
+int e = 0;
+while (e < 10)
 {
-if (c % 3 == 0 || c % 5 == 0)
+d = 0;
+while (d < 10)
 {
-sum += c;
+c = 10;
+while (c < 10)
+{
+if (c != d && e < d && d < c)
+{
+putchar ('0' + e);
+putchar ('0' + d);
+putchar ('0' + c);
+if (c + d + e != 9 + 8 + 7)
+{
+putchar(',');
+putchar(' ');
+}
 }
 c++;
 }
-printf("%i\n", sum);
+d++;
+}
+e++;
+}
+putchar('\n');
 return (0);
 }
